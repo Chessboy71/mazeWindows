da2675da4f67a679cbe9c2282068935d40a45e79
chessboy71 <bentegrimohamed@gmail.com>
2024-05-26 23:19:34 +0200

    Add project files.

---
+++ mazeWindows/utils.h
@@ -0,0 +1,19 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <SDL.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+/**
+Main functions 
+*/
+
+bool init();
+void close();
+bool LoadMedia();
+
+
+
+#endif /* !UTILS_H */
+
